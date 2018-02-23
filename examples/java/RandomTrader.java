5a7eeadbd37188c1b857a4206a7af42f4c2310fb
Maksim Solovjov <himaxx2@gmail.com>
2018-02-23 00:59:23 +0000

    Extract trading into a separate function

--- examples/java/RandomTrader.java
+++ examples/java/RandomTrader.java
@@ -7,6 +7,27 @@ import java.util.concurrent.ThreadLocalRandom;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class RandomTrader {
+    public static void trade(FXBattleClient client) throws UnirestException, InterruptedException {
+        JSONObject acc = client.account();
+
+        if (acc.has("error")) {
+            System.out.println(acc.getString("error"));
+        }
+        else if (acc.has("USD") && acc.getDouble("USD") > 1) {
+            acc = client.sell("USDGBP", acc.getDouble("USD"));
+        }
+        else if (acc.has("GBP") && acc.getDouble("GBP") > 1) {
+            acc = client.sell("GBPUSD", acc.getDouble("GBP"));
+        }
+        else {
+            System.out.println("You're broke!");
+        }
+
+        if (acc.has("error")) {
+            System.out.println("Failed to sell: " + acc.getString("error"));
+        }
+    }
+
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Usage: java -jar trader.jar http://127.0.0.1:8080 api_key");
@@ -18,25 +39,7 @@ public class RandomTrader {
 
         while (true) {
             try {
-                JSONObject acc = client.account();
-
-                if (acc.has("error")) {
-                    System.out.println(acc.getString("error"));
-                }
-                else if (acc.has("USD") && acc.getDouble("USD") > 1) {
-                    acc = client.sell("USDGBP", acc.getDouble("USD"));
-                }
-                else if (acc.has("GBP") && acc.getDouble("GBP") > 1) {
-                    acc = client.sell("GBPUSD", acc.getDouble("GBP"));
-                }
-                else {
-                    System.out.println("You're broke!");
-                }
-
-                if (acc.has("error")) {
-                    System.out.println("Failed to sell: " + acc.getString("error"));
-                }
-
+                trade(client);
                 Thread.sleep(ThreadLocalRandom.current().nextInt(100, 1000));
             }
             catch (UnirestException e) {
